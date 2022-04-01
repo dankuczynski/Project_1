0d8e5fe83ad00f908804d16701c7d73c071b44da
Lesley Rich <lesley.t.rich@gmail.com>
2022-03-31 22:35:44 -0700

    login and tickets html fixes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from werkzeug.utils import redirect
+
+from custom_exceptions.bad_employee_info import BadEmployeeInfo
 from utils.manage_connection import OperationalError, connect
 import os
 
@@ -7,6 +9,7 @@ from data_access_layer.employee_dao.employee_imp import EmployeeDAOImp
 from data_access_layer.ticket_dao.ticket_imp import TicketDAOImp
 from service_access_layer.employee_service_access_layer.employee_sal_imp import EmployeeSALImp
 from service_access_layer.ticket_service_access_layer.ticket_sal_imp import TicketSALImp
+
 
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
@@ -27,10 +30,10 @@ ticket_service = TicketSALImp(ticket_dao)
 
 @app.post("/body")
 def sending_http_request_with_body():
-    body: dict = request.get_json()
-    employee_id = employee_dao.employee_id_username_password_match(body['1'], body['2'])
-    employee_id_dictionary = {"employeeId" : employee_id}
-    return jsonify(employee_id_dictionary), 200
+        body: dict = request.get_json()
+        employee_id = employee_dao.employee_id_username_password_match(body['1'], body['2'])
+        employee_id_dictionary = {"employeeId" : employee_id}
+        return jsonify(employee_id_dictionary), 200
 
 
 @app.post("/create_and_view_tickets")
