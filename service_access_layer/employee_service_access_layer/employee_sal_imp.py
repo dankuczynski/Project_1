f0296af493d3a24b8da728f94044fe312cda23d1
Lesley Rich <lesley.t.rich@gmail.com>
2022-04-04 14:02:52 -0700

    work plz

--- service_access_layer/employee_service_access_layer/employee_sal_imp.py
+++ service_access_layer/employee_service_access_layer/employee_sal_imp.py
@@ -1,25 +1,17 @@
-from custom_exceptions.bad_ticket_info import BadTicketInfo
-from service_access_layer.ticket_service_access_layer.ticket_sal_interface import TicketSALInterface
+from custom_exceptions.bad_employee_info import BadEmployeeInfo
+from entities.Employee import Employee
+from service_access_layer.employee_service_access_layer.employee_sal_interface import EmployeeSALInterface
 from utils.manage_connection import connection
 
 
-class TicketSALImp(TicketSALInterface):
-    def cancel_ticket(self, ticket_number: int) -> bool:
-        sql = "delete * from ticket where ticket_number = %s"
+class EmployeeSALImp(EmployeeSALInterface):
+    def user_account_access(self, username: str, password: str) -> Employee:
+        sql = "select password from employee where username = %s"
         cursor = connection.cursor()
-        cursor.execute(sql, [ticket_number])
-        connection.commit()
-        if cursor.rowcount != 0:
-            return True
+        cursor.execute(sql, [username])
+        record = cursor.fetchone()
+        if record is not None:
+            employee = Employee(*record)
+            return employee
         else:
-            raise BadTicketInfo("No ticket found with that ticket number")
-
-    def create_ticket(self, ticket: Ticket) -> Ticket:
-        if type(ticket.ticket_number) != int:
-            raise BadTicketInfo("")
-        if type(ticket.employee_id) != int:
-            raise BadTicketInfo("")
-        if type(ticket.reimbursement_reason) != str:
-            raise BadTicketInfo("")
-        if type(ticket.reimbursement_ticket_amount) != float:
-            raise BadTicketInfo("")
+            raise BadEmployeeInfo("Invalid employee information")
