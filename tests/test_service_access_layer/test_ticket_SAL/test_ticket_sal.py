31f5024684970246564f786c319f8f382a0b5f80
jthapa25 <jeny860@revature.net>
2022-04-05 13:24:25 -0500

    add more info to test ticket sal

--- tests/test_service_access_layer/test_ticket_SAL/test_ticket_sal.py
+++ tests/test_service_access_layer/test_ticket_SAL/test_ticket_sal.py
@@ -1,11 +1,57 @@
+from custom_exceptions.bad_ticket_info import BadTicketInfo
 from custom_exceptions.cancellation_fail import CancellationFail
 from data_access_layer.ticket_dao.ticket_imp import TicketDAOImp
 from entities.Ticket import Ticket
 from service_access_layer.ticket_service_access_layer.ticket_sal_imp import TicketSALImp
+
 ticket_dao = TicketDAOImp()
 ticket_service = TicketSALImp(ticket_dao)
+non_int_ticket_number = Ticket("A", 20, "I need ticket for food", 50.00)
+non_int_employee_id = Ticket(1, "Two", "I need ticket for food", 50.00)
+non_str_reimbursement_reason = Ticket(1, 2, 1000, 50.00)
+non_float_ticket_amount = Ticket(1, 2, "I need ticket for food", "Five")
 
 
+def test_check_non_int_ticket_number():
+    try:
+        ticket_service.create_ticket(non_int_ticket_number)
+        assert False
+    except BadTicketInfo as e:
+        assert str(e) == "Please pass valid ticket number"
+
+
+def test_check_non_int_employee_id():
+    try:
+        ticket_service.create_ticket(non_int_employee_id)
+        assert False
+    except BadTicketInfo as e:
+        assert str(e) == "Please pass valid employee Id"
+
+
+def test_check_non_str_reimbursement_reason():
+    try:
+        ticket_service.create_ticket(non_str_reimbursement_reason)
+        assert False
+    except BadTicketInfo as e:
+        assert str(e) == "Please pass valid reimbursement reason"
+
+
+def test_check_reimbursement_reason_length():
+    try:
+        pass
+    except:
+        pass
+
+
+def test_check_non_float_ticket_amount():
+    try:
+        ticket_service.create_ticket(non_float_ticket_amount)
+        assert False
+    except BadTicketInfo as e:
+        assert str(e) == "Please pass valid ticket amount"
+
+
+"""
 def test_ticket_cancellation_success():
     try:
         ticket = Ticket(1, 20, "I need ticket for food", 50.00)
@@ -48,4 +94,5 @@ def test_ticket_cancellation_invalid_data_type_employee_id():
         ticket_service.cancel_ticket(ticket.employee_id)
         assert False
     except CancellationFail as e:
-        assert str(e) == "Provided invalid Employee Id"
+        assert str(e) == "Provided invalid Employee Id"  
+        """""
